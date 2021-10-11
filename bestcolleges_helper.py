bd2bad20cf551aa3c0802e6133c4f7ce52eed76a
~BlacKHawK~ <yashash.gaurav@gmail.com>
2021-10-11 13:09:43 -0400

    implemented lexical partial similarity match

--- bestcolleges_helper.py
+++ bestcolleges_helper.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
 
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+
+
 bestcolleges_data_path = ".output\\bestcolleges_careers.csv"
 
 bestcolleges_data = pd.read_csv(bestcolleges_data_path)
@@ -14,10 +18,15 @@ def view_all_careers():
         print(str(index + 1) + ". " + careers_name)
 
 
-def view_career_info_by_name(career_name):
-    if bestcolleges_data.career_name.str.contains(career_name).any():
+def view_career_info_by_name(searched_career):
+    closest_result = process.extractOne(
+        searched_career,
+        bestcolleges_data.career_name.to_list(),
+        scorer=fuzz.partial_ratio,
+    )
+    if len(closest_result) > 0:
         search_result_row = bestcolleges_data[
-            bestcolleges_data.career_name == career_name
+            bestcolleges_data.career_name == closest_result[0]
         ]
         print(
             "Here is some guidance on " + search_result_row.career_name.iloc[0]
@@ -28,7 +37,9 @@ def view_career_info_by_name(career_name):
             print(search_result_row.career_info.iloc[0])
 
         if search_result_row.why_career is not np.nan:
-            print("----  Why " + search_result_row.career_name + " ----")
+            print(
+                "----  Why " + search_result_row.career_name.iloc[0] + " ----"
+            )
             print(search_result_row.why_career.iloc[0])
 
         if search_result_row.how_to_start.iloc[0] is not np.nan:
