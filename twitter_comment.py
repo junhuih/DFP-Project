4a69a306a0dcdfa13ff867a16192a58831f8069c
Mark He <a.s.t.tobiichiorigami@gmail.com>
2021-10-12 19:25:04 -0400

    Add fail safe for twitter comment

--- twitter_comment.py
+++ twitter_comment.py
@@ -9,16 +9,21 @@ bearer_token = "AAAAAAAAAAAAAAAAAAAAALw%2FTwEAAAAA5mGzkkQRzM5lUExaBkZDfYbS3Y0%3D
 
 # Get the top twitter comment from twitter through API
 def getTwitterComments(school_name):
-    url = "https://api.twitter.com/2/tweets/search/recent?query="
-    url = url + school_name + '&tweet.fields=created_at'
-
-    headers = {"Authorization": "Bearer {}".format(bearer_token)}
-    response = requests.request("GET", url, headers=headers)
-    if response is None:
-        print("No tweets for "+school_name)
-    else:
-        try:
-            res_json = response.json()
-            print(res_json['data'][0]['text'])
-        except:
+    try:
+        url = "https://api.twitter.com/2/tweets/search/recent?query="
+        url = url + school_name + '&tweet.fields=created_at'
+    
+        headers = {"Authorization": "Bearer {}".format(bearer_token)}
+        response = requests.request("GET", url, headers=headers)
+        if response is None:
             print("No tweets for "+school_name)
+        else:
+            try:
+                res_json = response.json()
+                print(res_json['data'][0]['text'])
+                print(res_json['data'][1]['text'])
+                print(res_json['data'][2]['text'])
+            except:
+                print("No tweets for "+school_name)
+    except:
+        print("Error fetching twitter comments! Please check your internet!")
